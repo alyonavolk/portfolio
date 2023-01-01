f50742178385b35e52b859900741d91ee457441d
Alyona Volkova <alena18252830@gmail.com>
2023-01-01 23:49:39 +0500

    Добаление смены темы
    Смена темы с помощью useState и data-theme (не изменяется background у body).
    Возможна смена темы с полным изменением содержимого переменнных с помощью useEffect.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Main from '../Main/main';
@@ -6,10 +6,14 @@ import Background from '../subComponents/background/background';
 import Header from '../subComponents/header/header';
 
 
+
 function App() {
+  const [theme, setTheme] = useState(true);
+
+
   return (
     <Router>
-      <div className="app">
+      <div className="app" data-theme={`${theme ? 'default' : 'light'}`}>
         <div className='app__back'>
           <Background />
         </div>
@@ -22,6 +26,7 @@ function App() {
               </div>
             </Route>
           </Switch>
+          <button onClick={() => setTheme(!theme)}>theme</button>
         </div>
       </div>
     </Router>
--- src/components/subComponents/background/background.js
+++ src/components/subComponents/background/background.js
@@ -3,18 +3,18 @@ import './background.scss';
 
 const Background = () => {
     return (
-        <div className='poligon'>
-            <div className='poligon_11'>s</div>
-            <div className='poligon_10'>s</div>
-            <div className='poligon_9'>s</div>
-            <div className='poligon_8'>s</div>
-            <div className='poligon_7'>s</div>
-            <div className='poligon_6'>s</div>
-            <div className='poligon_5'>s</div>
-            <div className='poligon_4'>s</div>
-            <div className='poligon_3'>s</div>
-            <div className='poligon_2'>s</div>
-            <div className='poligon_1'>f</div>
+        <div className='polygon'>
+            <div className='polygon_11'>s</div>
+            <div className='polygon_10'>s</div>
+            <div className='polygon_9'>s</div>
+            <div className='polygon_8'>s</div>
+            <div className='polygon_7'>s</div>
+            <div className='polygon_6'>s</div>
+            <div className='polygon_5'>s</div>
+            <div className='polygon_4'>s</div>
+            <div className='polygon_3'>s</div>
+            <div className='polygon_2'>s</div>
+            <div className='polygon_1'>f</div>
         </div>
     );
 };
